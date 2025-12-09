4a2dcb36be22aefd835ace571a58df1616144313
Fogold <scottramsaypaton@gmail.com>
2025-12-09 11:08:45 +0000

    Updating project to line up with Supabase

--- listen.js
+++ listen.js
@@ -1,11 +1,11 @@
 const { app } = require("./app.js");
 
-const { port = 8080 } = process.env;
+const { PORT = 8080 } = process.env;
 
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Listening on" + port);
+    console.log("Listening on" + PORT);
   }
 });
